b55edffca52287491b4cb827b6204a792a7f86ed
Asilah Mohd Razak <148403421+asilahmr@users.noreply.github.com>
2026-01-14 06:47:20 +0800

    update admin password

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,7 @@ INSERT IGNORE INTO
 VALUES (
         'admin1@gmail.com',
         'Admin',
-        '12345678',
+        '$2a$10$wMLcvSMXjcAHXoxVss9s1eJEsJ1V2B961M3rWw/tTI1GAXI46AjOG',
         'admin',
         'active'
     );
